ef22391117c5fc01ffa0a4ebbcfed70d0976da09
Michael W Powell <mwpowellhtx@gmail.com>
2018-06-26 15:44:40 -0400

    added common assembly informational bits

---
+++ src/tools/Common/CommonAssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BumpAssemblyVersions.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
