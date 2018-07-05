c07df36115ededd98f0420326f890af431c67d84
Michael W Powell <mwpowellhtx@gmail.com>
2018-07-05 16:50:06 -0400

    did a bit of investigatory work not a final version whatsoever

--- src/BumpAssemblyVersions/BumpVersionTask.cs
+++ src/BumpAssemblyVersions/BumpVersionTask.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.Build.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.Build.Definition;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Evaluation;
 
 namespace Bav
 {
     using Microsoft.Build.Utilities;
+    using static String;
     using static MessageImportance;
 
     /// <summary>
@@ -11,6 +19,12 @@ namespace Bav
     /// <inheritdoc />
     public partial class BumpVersion : Task
     {
+        private AssemblyInfoBumpVersionService AssemblyInfoBumpVersion { get; } = new AssemblyInfoBumpVersionService();
+
+        public BumpVersion()
+        {
+        }
+
         /// <summary>
         /// 
         /// </summary>
@@ -18,6 +32,12 @@ namespace Bav
         /// <inheritdoc />
         public override bool Execute()
         {
+            // TODO: TBD: ... ostensibly, this is how we get at the project and CSharp files contained within...
+            var project = Project.FromFile(BuildEngine.ProjectFileOfTaskNode, new ProjectOptions { });
+            var csharpFiles = project.GetItems("Compile")
+                .Select(item => Path.Combine(project.DirectoryPath, item.EvaluatedInclude))
+                .Where(fullPath => fullPath.EndsWith(".cs")).ToArray();
+
             //throw new InvalidOperationException("unknown error occurred...");
 
             Log.LogMessage(High, $"Running {nameof(Execute)}");
