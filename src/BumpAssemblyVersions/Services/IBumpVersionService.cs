be545d2cebf800072767edb773bdd840530f5c4d
Michael W Powell <mwpowellhtx@gmail.com>
2018-07-14 17:07:42 -0400

    added bump version service interfaces

---
+++ src/BumpAssemblyVersions/Services/IBumpVersionService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Bav
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IBumpVersionService
+    {
+        /// <summary>
+        /// Gets the Mode.
+        /// </summary>
+        ServiceMode Mode { get; }
+
+        /// <summary>
+        /// Gets the set of VersionProviders.
+        /// </summary>
+        IEnumerable<IVersionProvider> VersionProviders { get; }
+    }
+}
---
+++ src/BumpAssemblyVersions/Services/IStreamBumpVersionService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Bav
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <inheritdoc cref="IBumpVersionService" />
+    public interface IStreamBumpVersionService : IBumpVersionService, IDisposable
+    {
+        /// <summary>
+        /// Gets the AttributeRegexes concerning the Service.
+        /// </summary>
+        IEnumerable<Regex> AttributeRegexes { get; }
+    }
+}
