d04ea6b13eabd2d28c4b65292f346dc5d11ffbfc
Michael W Powell <mwpowellhtx@gmail.com>
2018-07-14 17:09:31 -0400

    added attribute usage extension methods

---
+++ src/BumpAssemblyVersions/Extensions/AttributeUsageExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Bav
+{
+    internal static class AttributeUsageExtensionMethods
+    {
+        private static readonly Type AttributeType = typeof(Attribute);
+
+        internal static string ToLongName(this Type type) => type.Name;
+
+        internal static string ToShortName(this Type type)
+            => !type.Name.EndsWith(AttributeType.Name)
+                ? type.Name
+                : type.Name.Substring(0, type.Name.Length - AttributeType.Name.Length);
+    }
+}
