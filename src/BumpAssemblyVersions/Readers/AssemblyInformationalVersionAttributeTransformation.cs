c06f73ac3cd56ab3e50445fae437978a497ed0fc
Michael W Powell <mwpowellhtx@gmail.com>
2018-07-16 15:44:23 -0400

    added unused remarks

--- src/BumpAssemblyVersions/Readers/AssemblyInformationalVersionAttributeTransformation.cs
+++ src/BumpAssemblyVersions/Readers/AssemblyInformationalVersionAttributeTransformation.cs
@@ -2,9 +2,11 @@
 
 namespace Bav
 {
+    // ReSharper disable once UnusedMember.Global
     internal class AssemblyInformationalVersionAttributeTransformation
         : NuGetVersionAssemblyAttributeTransformationBase<AssemblyInformationalVersionAttribute>
     {
+        // ReSharper disable once UnusedMember.Global
         internal AssemblyInformationalVersionAttributeTransformation(string sourcePath)
             : base(sourcePath)
         {
