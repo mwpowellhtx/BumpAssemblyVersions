13d1ae2fe6d00ca81e6488fcf23cddf58ab7fee4
Michael W Powell <mwpowellhtx@gmail.com>
2018-07-19 15:04:50 -0400

    started adding some tests but may circle around on this later on

---
+++ src/BumpAssemblyVersions.Tests/Integration/AssemblyFileVersionStreamBumpVersionServiceAttributeRegexTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace Bav.Integration
+{
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class AssemblyInfoAttributeBumpVersionTests
+    {
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        ///// <param name="givenLines"></param>
+        ///// <param name="bumpVersionService"></param>
+        //[Theory]
+        //public virtual void Verify_Should_Bump(IEnumerable<string> givenLines, IBumpVersionService bumpVersionService)
+        //{
+        //    //bumpVersionService.
+        //}
+    }
+}
