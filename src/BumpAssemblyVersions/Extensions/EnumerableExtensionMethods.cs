9da2c3180571b18efc82a58d1d2a6630031bd99b
Michael W Powell <mwpowellhtx@gmail.com>
2018-07-19 14:59:38 -0400

    added an add to extension method

---
+++ src/BumpAssemblyVersions/Extensions/EnumerableExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Bav
+{
+    internal static class EnumerableExtensionMethods
+    {
+        internal static T AddTo<T>(this T value, IList<T> values)
+        {
+            values.Add(value);
+            return value;
+        }
+    }
+}
