0aa3adb5ab771d6a5540cc0d7a30c0931316cbf2
Michael W Powell <mwpowellhtx@gmail.com>
2018-07-19 15:00:33 -0400

    refactored some bits into this bump version service

--- src/BumpAssemblyVersions/Services/AssemblyInfoBumpVersionService.cs
+++ src/BumpAssemblyVersions/Services/AssemblyInfoBumpVersionService.cs
@@ -1,159 +1,130 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace Bav
 {
     using static String;
+    using static RegexOptions;
 
-    internal class AssemblyInfoBumpVersionService<T> : StreamBumpVersionServiceBase<T>
+    internal class AssemblyInfoBumpVersionService<[
+                ConstrainGenericTypes(typeof(AssemblyVersionAttribute)
+                    , typeof(AssemblyFileVersionAttribute)
+                    , typeof(AssemblyInformationalVersionAttribute))]
+            T>
+        : BumpVersionServiceBase, IAssemblyInfoBumpVersionService
         where T : Attribute
     {
-        // TODO: TBD: get set to consider how to receive specs from the end user...
-        internal AssemblyInfoBumpVersionService(params IVersionProvider[] versionProviders)
-            : base(versionProviders)
-        {
-        }
-
-        // TODO: TBD: this one may serve for both the CSharp source file(s) as well as for the project file...
-        // TODO: TBD: instead of reading by line, may read the whole file and split the lines out...
-        // TODO: TBD: we would want the whole text for purposes of the CSPROJ Xml document parsing...
         /// <summary>
-        /// Returns each of the lines from the <paramref name="assyInfoFullPath"/> sans any
-        /// Carriage Returns or Lines Feeds.
+        /// Returns the <see cref="ConstrainGenericTypesAttribute.AllowedTypes"/> specified
+        /// against the <see cref="Type"/> <typeparamref name="T"/>. Note the subtle difference
+        /// in angle brackets usage. We do not want the type specification in this instance.
         /// </summary>
-        /// <param name="assyInfoFullPath"></param>
         /// <returns></returns>
-        private static IEnumerable<string> ReadLinesFromFile(string assyInfoFullPath)
-        {
-            const FileMode mode = FileMode.Open;
-            const FileAccess access = FileAccess.Read;
-            const FileShare share = FileShare.ReadWrite;
+        private static IEnumerable<Type> GetSupportedAttributeTypes()
+            => typeof(AssemblyInfoBumpVersionService<>).GetGenericArguments()
+                .First().GetCustomAttributes<ConstrainGenericTypesAttribute>()
+                .SelectMany(constraint => constraint.AllowedTypes);
 
-            using (var fs = File.Open(assyInfoFullPath, mode, access, share))
+        // ReSharper disable once StaticMemberInGenericType
+        /// <summary>
+        /// Gets the set of SupportedAttributeTypes based on the
+        /// <see cref="ConstrainGenericTypesAttribute"/> decoration.
+        /// </summary>
+        protected internal static IEnumerable<Type> SupportedAttributeTypes { get; }
+            = GetSupportedAttributeTypes().ToArray();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected static readonly Type AttributeType = typeof(T);
+
+        // TODO: TBD: there may be a base class AttributeRegexes that should be discovered...
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        protected static IEnumerable<Regex> GetAttributeRegexes()
+        {
+            // We will return the naive pattern matching for faster identification.
+            string GetRegexPattern(string attribName)
             {
-                return ReadLinesFromStream(fs);
+                /*
+                 * Went with an abbreviated notation here for brevity:
+                 * wc = wildcard
+                 * v = version
+                 * s = semantic
+                 * e = element
+                 * x = extended, meaning dot delimited
+                 */
+                const string dot = @"\.";
+                const string hyp = @"\-";
+                var wc = $@"({dot}\*)";
+                const string ve = @"(\d)+";
+                var xve = $@"({dot}{ve})";
+                var version = $@"(?<version>{ve}{xve}({wc}|{xve}{wc}?|{xve}{{2}})?)";
+                var se = $@"(a-zA-Z\d{hyp})+";
+                var xse = $@"({dot}{se})";
+                var semantic = $"(?<semantic>{se}{xse}*)";
+                return $@"\[assembly\: {attribName}\(""{version}({hyp}{semantic})?""\)\]";
             }
+
+            // There may be instances where it does not quite match the pattern.
+            var names = new[] {AttributeType.ToShortName(), AttributeType.ToLongName()}.Distinct().ToArray();
+
+            return names.Select(name => new Regex(GetRegexPattern(name), Compiled));
         }
 
-        private static void WriteLinesToFile(string assyInfoFullPath, IEnumerable<string> lines)
-        {
-            const FileMode mode = FileMode.Create;
-            const FileAccess access = FileAccess.Write;
-            const FileShare share = FileShare.Read;
+        /// <inheritdoc />
+        public IEnumerable<Regex> AttributeRegexes { get; } = GetAttributeRegexes().ToArray();
 
-            using (var fs = File.Open(assyInfoFullPath, mode, access, share))
+        static AssemblyInfoBumpVersionService()
+        {
+            if (SupportedAttributeTypes.Any(type => type == AttributeType))
             {
-                WriteLinesToStream(fs, lines.ToArray());
+                return;
             }
+
+            var supported = Join(", ", SupportedAttributeTypes.Select(type => $"'{type.FullName}'"));
+
+            throw new InvalidOperationException(
+                $"'{AttributeType.FullName}' is not supported"
+                + $" by '{typeof(AssemblyInfoBumpVersionService<T>)}': {supported}."
+            );
         }
 
         /// <summary>
-        /// Returns whether the file on the other end of the <paramref name="assyInfoFullPath"/>
-        /// Contains any of the <paramref name="results"/>.
+        /// Returns whether the <paramref name="givenLines"/> Contains Any of the
+        /// <paramref name="results"/> identified by the <see cref="AttributeRegexes"/> set of
+        /// <see cref="Regex"/> pattern matchers.
         /// </summary>
-        /// <param name="assyInfoFullPath"></param>
+        /// <param name="givenLines"></param>
         /// <param name="results"></param>
         /// <returns></returns>
-        /// <see cref="StreamBumpVersionServiceBase{T}.ContainsAttribute"/>
-        private bool ContainsAttribute(string assyInfoFullPath, out IEnumerable<BumpMatch> results)
-            => ContainsAttribute(ReadLinesFromFile(assyInfoFullPath), out results);
+        protected bool ContainsAttribute(IEnumerable<string> givenLines, out IEnumerable<BumpMatch> results)
+            => (results = givenLines.Select(
+                        l => BumpMatch.Create(AttributeRegexes.FirstOrDefault(regex => regex.IsMatch(l))?.Match(l), l)
+                    )
+                ).Any(x => x.IsMatch);
 
-        private static bool VerifyServiceRequest(ServiceMode mode, IEnumerable<IVersionProvider> providers)
+        ///// <summary>
+        ///// Returns whether the file on the other end of the <paramref name="assyInfoFullPath"/>
+        ///// Contains any of the <paramref name="results"/>.
+        ///// </summary>
+        ///// <param name="assyInfoFullPath"></param>
+        ///// <param name="results"></param>
+        ///// <returns></returns>
+        ///// <see cref="StringBumpVersionServiceBase{T}.ContainsAttribute"/>
+        //private bool ContainsAttribute(string assyInfoFullPath, out IEnumerable<BumpMatch> results)
+        //    => ContainsAttribute(ReadLinesFromFile(assyInfoFullPath), out results);
+
+        /// <inheritdoc />
+        public bool TryBumpVersion(IEnumerable<string> givenLines, out IEnumerable<string> resultLines)
         {
-            // ReSharper disable once PossibleMultipleEnumeration
-            var p = providers.ToArray();
-
-            if (SupportedAttributeTypes.All(type => type != AttributeType))
-            {
-                throw new InvalidOperationException(
-                    $"The '{AttributeType.FullName}' is not among the supported types:"
-                    + $" {Join(", ", SupportedAttributeTypes.Select(type => $"'{type.FullName}'"))}");
-            }
-
-            // TODO: TBD: may let "mode" be a decision driven by VersionProviders ...
-            if (mode != ServiceMode.NoElements || !p.Any())
-            {
-                return false;
-            }
-
-            if ((mode & ServiceMode.ReleaseElements) != ServiceMode.ReleaseElements)
-            {
-                return true;
-            }
-
-            if (!p.OfType<PreReleaseIncrementVersionProvider>().Any())
-            {
-                throw new InvalidOperationException(
-                    $"Expected there to be a {typeof(PreReleaseIncrementVersionProvider).FullName}"
-                    + $" among the '{nameof(VersionProviders)}'.");
-            }
-
-            if (p.Any() && !(p.Last() is PreReleaseIncrementVersionProvider))
-            {
-                throw new InvalidOperationException(
-                    $"Expected the {typeof(PreReleaseIncrementVersionProvider).FullName} to be the"
-                    + $" last of the enumerated '{nameof(VersionProviders)}'.");
-            }
-
-            // Clear to proceed, at least as far as up front Verification is concerned.
-            return true;
-        }
-
-        /// <summary>
-        /// Tries to Bump the Version contained by the <paramref name="assyInfoFullPath"/>
-        /// indicated by the <typeparamref name="T"/> <see cref="Attribute"/>.
-        /// </summary>
-        /// <param name="assyInfoFullPath"></param>
-        /// <returns></returns>
-        internal bool TryBumpVersion(string assyInfoFullPath)
-        {
-            // Rule out several obvious scenarios prior to more intrusive vetting.
-            if (!VerifyServiceRequest(Mode, VersionProviders))
-            {
-                return false;
-            }
-
-            /* Rule out any configuration related issues prior to touching any
-             of the potentially involved files in any substantive way. */
-            if (!ContainsAttribute(assyInfoFullPath, out var candidates))
-            {
-                return false;
-            }
-
-            const string version = nameof(version);
-
-            // ReSharper disable once LoopCanBeConvertedToQuery it's not just this LINQ query, we want TODO more with it...
-            // We aligned with the Candidates alread.
-            // ReSharper disable once PossibleMultipleEnumeration
-            foreach (var candidate in candidates.Where(x => x.IsMatch))
-            {
-                // TODO: TBD: instead of looking up the Regex all over again, just utilize the Match we discovered during the parse...
-
-                // ReSharper disable once LoopCanBeConvertedToQuery ditto TODO...
-                // Now we want to align with the Regexes as well.
-                foreach (var regex in AttributeRegexes.Where(r => r.IsMatch(candidate.Line)))
-                {
-                    var match = regex.Match(candidate.Line);
-
-                    var versionString = match.Groups[version].Value;
-
-                    // TODO: TBD: this is where we want toe bits from the Match/Groups ...
-
-
-                    // TODO: TBD: now that we actually have the Version String, then we must connect it somehow with the Version Provider(s)...
-
-                    // TODO: TBD: this is where we must "parse" the version string, including comprehension of wildcards, possibly also comprehension of release labels, metadata, etc
-                    // TODO: TBD: probably also with decision making driven by the VersionProviders...
-
-                    return true;
-                }
-            }
-
-            // ReSharper disable once PossibleMultipleEnumeration
-            WriteLinesToFile(assyInfoFullPath, candidates.Select(x => x.Line));
-
+            resultLines = givenLines.ToArray();
+            // TODO: TBD: examine the lines, may inject the Reflection using statement, bump the version, etc...
             return false;
         }
     }
---
+++ src/BumpAssemblyVersions/Services/IAssemblyInfoBumpVersionService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Bav
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <inheritdoc cref="IBumpVersionService" />
+    public interface IAssemblyInfoBumpVersionService : IBumpVersionService
+    {
+        /// <summary>
+        /// Gets the AttributeRegexes concerning the Service.
+        /// </summary>
+        IEnumerable<Regex> AttributeRegexes { get; }
+
+        /// <summary>
+        /// Tries to Bump the Version in the <paramref name="givenLines"/> resulting in the
+        /// <paramref name="resultLines"/>.
+        /// </summary>
+        /// <param name="givenLines"></param>
+        /// <param name="resultLines"></param>
+        /// <returns></returns>
+        bool TryBumpVersion(IEnumerable<string> givenLines, out IEnumerable<string> resultLines);
+    }
+}
