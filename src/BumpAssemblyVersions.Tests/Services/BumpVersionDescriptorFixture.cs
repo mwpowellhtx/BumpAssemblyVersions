cf8d690fe04c3d628558737e0afadd3277bbb030
Michael W Powell <mwpowellhtx@gmail.com>
2018-07-24 11:57:27 -0400

    updated fixture interface

---
+++ src/BumpAssemblyVersions.Tests/Services/BumpVersionDescriptorFixture.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Bav
+{
+    // ReSharper disable once PartialTypeWithSinglePart
+    /// <summary>
+    /// Internally we will provide <see cref="IVersionProvider"/> Templates for test purposes.
+    /// </summary>
+    internal partial class BumpVersionDescriptorFixture : BumpVersionDescriptor
+    {
+        /// <summary>
+        /// Sets the <see cref="BumpVersionDescriptor.DescriptorTimestamp"/> to the Value for
+        /// Internal test purposes.
+        /// </summary>
+        internal DateTime InternalDescriptorTimestamp
+        {
+            set => DescriptorTimestamp = value;
+        }
+
+        /// <summary>
+        /// Internal Constructor.
+        /// </summary>
+        /// <param name="majorProviderTemplate"></param>
+        /// <param name="minorProviderTemplate"></param>
+        /// <param name="patchProviderTemplate"></param>
+        /// <param name="buildProviderTemplate"></param>
+        /// <param name="releaseProviderTemplate"></param>
+        internal BumpVersionDescriptorFixture(
+            IVersionProvider majorProviderTemplate = null
+            , IVersionProvider minorProviderTemplate = null
+            , IVersionProvider patchProviderTemplate = null
+            , IVersionProvider buildProviderTemplate = null
+            , IVersionProvider releaseProviderTemplate = null
+        )
+            : base(
+                majorProviderTemplate
+                , minorProviderTemplate
+                , patchProviderTemplate
+                , buildProviderTemplate
+                , releaseProviderTemplate
+            )
+        {
+        }
+    }
+}
