a869975c489c242b49bad62a92fe5c7346d1cd78
Michael W Powell <mwpowellhtx@gmail.com>
2018-08-03 17:33:39 -0400

    added the strategic event arguments supporting bump version service

---
+++ src/BumpAssemblyVersions/Events/BumpResultEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bav
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <inheritdoc />
+    public class BumpResultEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Gets the Result.
+        /// </summary>
+        public BumpResult Result { get; }
+
+        internal BumpResultEventArgs(BumpResult result)
+        {
+            Result = result;
+        }
+    }
+}
---
+++ src/BumpAssemblyVersions/Events/UsingStatementAddedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bav
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <inheritdoc />
+    public class UsingStatementAddedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string UsingStatement { get; }
+
+        internal UsingStatementAddedEventArgs(string usingStatement)
+        {
+            UsingStatement = usingStatement;
+        }
+    }
+}
