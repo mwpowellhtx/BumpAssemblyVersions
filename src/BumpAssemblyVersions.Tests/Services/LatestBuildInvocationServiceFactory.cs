1f505c961b97dd058d64569df0d4df963920c89a
Michael W Powell <mwpowellhtx@gmail.com>
2018-08-03 18:13:28 -0400

    extended out through latest build factory

---
+++ src/BumpAssemblyVersions.Tests/Services/LatestBuildInvocationServiceFactory.cs
@@ -0,0 +1,29 @@
+﻿namespace Bav
+{
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <inheritdoc />
+    public class LatestBuildInvocationServiceFactory : MSBuildInvocationServiceFactory<LatestBuildInvocationService>
+    {
+        /// <summary>
+        /// Public Constructor.
+        /// </summary>
+        /// <param name="outputHelper"></param>
+        /// <inheritdoc />
+        public LatestBuildInvocationServiceFactory(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="outputHelper"></param>
+        /// <returns></returns>
+        public override LatestBuildInvocationService GetService(ITestOutputHelper outputHelper)
+            => new LatestBuildInvocationService(outputHelper, Verbosity);
+    }
+}
