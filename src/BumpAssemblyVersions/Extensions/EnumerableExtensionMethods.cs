77cf668de57f18b543a0f99adb5650de9a1dcf9f
Michael W Powell <mwpowellhtx@gmail.com>
2018-08-18 22:15:22 -0400

    added a couple of strategic enumerable extension methods

--- src/BumpAssemblyVersions/Extensions/EnumerableExtensionMethods.cs
+++ src/BumpAssemblyVersions/Extensions/EnumerableExtensionMethods.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Bav
 {
@@ -9,5 +11,26 @@ namespace Bav
             values.Add(value);
             return value;
         }
+
+        /// <summary>
+        /// Returns whether <paramref name="value"/> IsOneOf the <paramref name="values"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        internal static bool ContainedBy<T>(this T value, params T[] values) => values.Contains(value);
+
+        /// <summary>
+        /// Returns whether <paramref name="value"/> IsOneOf the <paramref name="values"/> using
+        /// the <paramref name="equals"/> comparison.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <param name="equals"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        internal static bool ContainedBy<T>(this T value, Func<T, T, bool> equals, params T[] values)
+            => values.Any(x => equals.Invoke(value, x));
     }
 }
