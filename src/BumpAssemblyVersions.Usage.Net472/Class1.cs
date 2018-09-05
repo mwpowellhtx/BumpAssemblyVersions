81f22343aba89eecdea816238b2a6252d202b19c
Michael W Powell <mwpowellhtx@gmail.com>
2018-09-05 16:02:36 -0400

    added usage project testing locally deployed package

---
+++ src/BumpAssemblyVersions.Usage.Net472/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BumpAssemblyVersions.Usage.Net472
+{
+    public class Class1
+    {
+    }
+}
