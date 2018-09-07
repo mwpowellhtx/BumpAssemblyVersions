179cb38edaa0216a675a83a9b0020f467da3973f
Michael W Powell <mwpowellhtx@gmail.com>
2018-09-06 22:13:49 -0400

    updated assembly version with latest changes tested

--- src/TestSolution/AssyInfo.FileInfoVersions.IncludeWildcards/Properties/AssemblyInfo.cs
+++ src/TestSolution/AssyInfo.FileInfoVersions.IncludeWildcards/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 //[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.43.*")]
-[assembly: AssemblyFileVersion("1.0.43.0")]
+[assembly: AssemblyVersion("1.0.44.*")]
+[assembly: AssemblyFileVersion("1.0.44.0")]
 
-[assembly: AssemblyInformationalVersion("1.0.40")]
+[assembly: AssemblyInformationalVersion("1.0.41")]
