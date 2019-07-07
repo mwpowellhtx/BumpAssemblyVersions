8c2d4624dfc6d8efccb2faac26683aa9e339d9e7
Michael W Powell <mwpowellhtx@gmail.com>
2019-07-07 13:41:28 -0400

    updated unit test assets

--- src/BumpAssemblyVersions.Tests/Services/AssemblyInfoBumpVersionServiceTests.Supported.cs
+++ src/BumpAssemblyVersions.Tests/Services/AssemblyInfoBumpVersionServiceTests.Supported.cs
@@ -11,18 +11,22 @@ namespace Bav
     public abstract partial class AssemblyInfoBumpVersionServiceTests<T>
     {
         /// <summary>
-        /// Verify that the
-        /// <see cref="AssemblyInfoBumpVersionService{T}.SupportedAttributeTypes"/> themselves
-        /// are accurate and accounted for among the <paramref name="expectedSupportedTypes"/>.
+        /// Verify that the <see cref="AssemblyInfoBumpVersionService.SupportedAttributeTypes"/>
+        /// themselves are accurate and accounted for among the
+        /// <paramref name="expectedSupportedTypes"/>.
         /// </summary>
         /// <param name="expectedSupportedTypes"></param>
         [Theory, MemberData(nameof(ExpectedSupportedTypes))]
         public void Verify_Supported_Attribute_Type(params Type[] expectedSupportedTypes)
         {
-            const string propertyName = nameof(AssemblyInfoBumpVersionServiceFixture<T>.SupportedAttributeTypes);
+            const string propertyName = nameof(AssemblyInfoBumpVersionService.SupportedAttributeTypes);
+
+            /* We must do it this way in order for the property to be seen properly, which
+             * exists in a Base Class, but we want to see it through a Derived Class. */
+            const BindingFlags propertyFlags = Static | NonPublic | GetProperty | FlattenHierarchy;
 
             // Not technically from the FixtureType, per se.
-            var property = typeof(AssemblyInfoBumpVersionService<T>).GetProperty(propertyName, Static | NonPublic | GetProperty);
+            var property = typeof(AssemblyInfoBumpVersionService<T>).GetProperty(propertyName, propertyFlags);
 
             Assert.NotNull(property);
             Assert.Equal(typeof(IEnumerable<Type>), property.PropertyType);
@@ -30,7 +34,7 @@ namespace Bav
             // While technically we could just Get the Value, let's also assert that we did.
             bool TryGetPropertyValue(PropertyInfo pi, out IEnumerable<Type> types)
             {
-                types = (IEnumerable<Type>)pi.GetValue(null);
+                types = (IEnumerable<Type>) pi.GetValue(null);
                 return types != null && types.Any();
             }
 
