e8fd5b913c28553f87ba0637be01c503f62a6ce7
Michael W Powell <mwpowellhtx@gmail.com>
2019-08-19 18:56:39 -0400

    added to the interface also updated comments

--- src/BumpAssemblyVersions/IBumpVersionDescriptor.cs
+++ src/BumpAssemblyVersions/IBumpVersionDescriptor.cs
@@ -2,38 +2,46 @@
 
 namespace Bav
 {
+    using Microsoft.Build.Framework;
+
     /// <summary>
     /// 
     /// </summary>
     public interface IBumpVersionDescriptor
     {
         /// <summary>
-        /// 
+        /// Gets the BumpPath. Corresponds to the <see cref="ITaskItem.ItemSpec"/>
+        /// used to specify the item in the first place.
+        /// </summary>
+        string BumpPath { get; }
+
+        /// <summary>
+        /// Gets the Kind of Version.
         /// </summary>
         VersionKind Kind { get; }
 
         /// <summary>
-        /// 
+        /// Gets the Major Provider Template.
         /// </summary>
         IVersionProvider MajorProviderTemplate { get; }
 
         /// <summary>
-        /// 
+        /// Gets the Minor Provider Template.
         /// </summary>
         IVersionProvider MinorProviderTemplate { get; }
 
         /// <summary>
-        /// 
+        /// Gets the Patch Provider Template.
         /// </summary>
         IVersionProvider PatchProviderTemplate { get; }
 
         /// <summary>
-        /// 
+        /// Gets the Build Provider Template.
         /// </summary>
         IVersionProvider BuildProviderTemplate { get; }
 
         /// <summary>
-        /// 
+        /// Gets the Release Provider Template.
         /// </summary>
         IVersionProvider ReleaseProviderTemplate { get; }
 
