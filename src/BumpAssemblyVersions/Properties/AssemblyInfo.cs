540bc4d614ec3cf6418503d36b2ed4a5e292aa52
Michael W. Powell <mwpowelllde@gmail.com>
2020-07-04 22:43:22 -0400

    updated bump assembly properties and targets added a bit more clarity around debug targets redirected packaging through the NuGet specification itself with a little help from the project and some build time injected properties several disabled warnings later seems to work pretty well

--- src/BumpAssemblyVersions/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: Guid("61db48ac-dccd-4af8-9a55-2bd311484102")]
-
-[assembly: InternalsVisibleTo("BumpAssemblyVersions.Tests")]
