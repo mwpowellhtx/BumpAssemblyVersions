eb4961046a569196f7980e58e474e56b8db1ec1d
Michael W. Powell <mwpowelllde@gmail.com>
2020-07-04 22:45:29 -0400

    added 2020 delta days version provider

---
+++ src/BumpAssemblyVersions/Providers/DeltaDays2020VersionProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Bav
+{
+    /// <inheritdoc />
+    public class DeltaDays2020VersionProvider : DeltaDaysVersionProviderBase
+    {
+        private static DateTime GetBaseTimestamp() => new DateTime(2020, 1, 1);
+
+        /// <summary>
+        /// Gets the BaseTimestamp.
+        /// </summary>
+        /// <inheritdoc />
+        protected override DateTime BaseTimestamp { get; } = GetBaseTimestamp();
+
+        /// <summary>
+        /// Private Copy Constructor.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <inheritdoc />
+        // ReSharper disable once SuggestBaseTypeForParameter, UnusedMember.Local
+        private DeltaDays2020VersionProvider(DeltaDays2020VersionProvider other)
+            : base(other)
+        {
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        /// <summary>
+        /// Internal Default Constructor.
+        /// </summary>
+        /// <inheritdoc />
+        internal DeltaDays2020VersionProvider()
+        {
+        }
+    }
+}
