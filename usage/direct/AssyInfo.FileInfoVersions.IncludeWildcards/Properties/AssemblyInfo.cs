99cfd10f3558a26809ce45aaa0e90fcc272de41e
Michael W. Powell <mwpowelllde@gmail.com>
2020-11-16 11:38:16 -0500

    Added direct usage build properties. Updated existing projects with build instructions.

--- usage/direct/AssyInfo.FileInfoVersions.IncludeWildcards/Properties/AssemblyInfo.cs
+++ usage/direct/AssyInfo.FileInfoVersions.IncludeWildcards/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 //[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.49.*")]
-[assembly: AssemblyFileVersion("1.0.49.0")]
+[assembly: AssemblyVersion("1.0.50.*")]
+[assembly: AssemblyFileVersion("1.0.50.0")]
 
-[assembly: AssemblyInformationalVersion("1.0.46")]
+[assembly: AssemblyInformationalVersion("1.0.47")]
